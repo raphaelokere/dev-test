c5562fcd68705f1fc104c5137b12370178b46c29
Raphael Okere <Raphael.Okere@Honeywell.com>
2022-07-13 06:16:22 -0400

    updating provider

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.13.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
 
 resource "azurerm_resource_group" "example" {
