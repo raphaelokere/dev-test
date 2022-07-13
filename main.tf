c31444b909349c2270fa43aac8de2916b2300c69
Raphael Okere <Raphael.Okere@Honeywell.com>
2022-07-13 06:14:15 -0400

    update

--- main.tf
+++ main.tf
@@ -1,74 +1,28 @@
-### Un-comment to use the following resource blocks to provision new Resource Groups, Vnets, Subnets ###
-# resource "azurerm_resource_group" "rg" {
-#   name     = var.azurerm_resource_group
-#   location = var.azurerm_location
-# }
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = var.azurerm_vnet
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = var.azurerm_location
-#   address_space       = var.address_space
-# }
-# resource "azurerm_subnet" "subnet" {
-#   name                 = var.azurerm_subnet
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = var.address_prefixes
-# }
-
-### Datasource example for already existing resources ###
-
-data "azurerm_resource_group" "rg" {
-  name = "${var.azurerm_resource_group}"
+provider "azurerm" {
+  features {}
 }
 
-data "azurerm_virtual_network" "vnet" {
-  name = "${var.azurerm_vnet}"
-  resource_group_name = "${var.azurerm_resource_group}"
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "East US"
 }
 
-data "azurerm_subnet" "subnet" {
-  name = "${var.azurerm_subnet}"
-  virtual_network_name = "${var.azurerm_vnet}"
-  resource_group_name = "${var.azurerm_resource_group}"
-}
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-module "virutal_machine" {
-source = "git::https://H507669@bitbucket.honeywell.com/scm/~h507669/sps-pss-devsecops.git//sps-pss-w-azurevm"
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
 
-  # Resource Group, location, VNet and Subnet details
-  azurerm_resource_group             = data.azurerm_resource_group.rg.name # If creating Azure resources, change this input argument to azurerm_resource_group.rg.name
-  azurerm_location                   = data.azurerm_resource_group.rg.location # If creating Azure resources, change this input argument to azurerm_resource_group.rg.location
-  azurerm_vnet                       = data.azurerm_virtual_network.vnet.name # If creating Azure resources, change this input argument to azurerm_virtual_network.vnet.name
-  azurerm_subnet                     = data.azurerm_subnet.subnet.name # If creating Azure resources, change this input argument to azurerm_subnet.subnet.name
-  azurerm_vnet_subnet_resource_group = data.azurerm_resource_group.rg.name # If creating Azure resources, change this input argument to azurerm_resource_group.rg.name
-  depends_on                         = [data.azurerm_subnet.subnet] # If creating Azure resources, change this input argument to [azurerm_subnet.subnet]
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 
-  #Placement group and availability sets
-  azurerm_placement_group_name       = var.azurerm_placement_group_name
-  azurerm_enable_vm_availability_set = true
-  azurerm_availability_set_name      = var.azurerm_availability_set_name
-
-  #Accelerated Network
-  azurerm_enable_accelerated_networking = true
-
-  # VM details
-  azurerm_vm_name              = var.azurerm_vm_name
-  azurerm_vm_count             = var.azurerm_vm_count
-  azurerm_vm_size              = var.azurerm_vm_size
-  azurerm_vm_os_disk_type      = var.azurerm_vm_os_disk_type
-  azurerm_vm_managed_disk_type = var.azurerm_vm_managed_disk_type
-  azurerm_vm_disk_size         = var.azurerm_vm_disk_size
-
-
-  #User Details
-  azurerm_vm_username = var.azurerm_vm_username
-  azurerm_vm_password = var.azurerm_vm_password
-
-  azurerm_enable_vm_data_disk = true
-  azurerm_vm_data_disk_size   = var.azurerm_vm_data_disk_size
-  azurerm_vm_data_disk_count  = var.azurerm_vm_data_disk_count
-
-  tags = var.tags
-
+  depends_on = [azurerm_resource_group.example]
 }
